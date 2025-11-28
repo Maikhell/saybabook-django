1c1623931d4fedb525827833635b44c12252b69e
John Michael Camara <johnmichael.camara.git@gmail.com>
2025-11-28 22:57:25 +0800

    Added new .html pages and routed them

--- saybabook_app/urls.py
+++ saybabook_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='home'),
+    path('browse/', views.browse_page, name ='browse'),
+    path('addbook/', views.add_book_page, name = 'addbooks'),
+    path('archive/', views.archive_page, name = 'archive')
 ]
--- saybabook_app/views.py
+++ saybabook_app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'saybabook_app/landingpage.html')
+
+def browse_page(request):
+    return render(request, 'saybabook_app/browse.html')
+
+def archive_page(request):
+    return render (request, 'saybabook_app/archive.html')
+
+def add_book_page(request):
+    return render(request, 'saybabook_app/addbook.html')
+
+def account_page(request):
+    return render(request, 'saybabook_app/account.html')
+
+def create(request):
+    return render(request, '', {'form'})
+
